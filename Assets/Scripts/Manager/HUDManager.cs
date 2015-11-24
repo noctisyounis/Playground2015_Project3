86bb212a11004b314ec43def82d48f8e7d6e195a
Kweena <michael.kweena.fossoul@gmail.com>
2015-11-24 11:53:14 +0100

    Add Inventory HUD

---
+++ Assets/Scripts/Manager/HUDManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDManager : MonoBehaviour {
+
+    public static SlotManager m_slotManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_slotManager.CreateSlots();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
