fee7da674cd8f912049ed7dbf8087e65b39b86fc
Kweena <michael.kweena.fossoul@gmail.com>
2015-11-24 16:51:51 +0100

    Fixed All

---
+++ Assets/Scripts/HUDScript/DamageScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class DamageScript : MonoBehaviour {
+
+    public int DamageOnAttack = 20;
+    GameObject Player;
+    HungryScript Hunger;
+
+    #region Debug
+    void OnGUI()
+    {
+        if (GUILayout.Button("damage"))
+        {
+            GameObject.Find("Koromon").GetComponent<HungryScript>().TakeDamage(DamageOnAttack);
+        }
+    } 
+    #endregion
+}
---
+++ Assets/Scripts/Manager/GameOverManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+
+
+public class GameOverManager : MonoBehaviour
+{
+
+    #region Public_Members
+    
+    public HungryScript m_CurrentHunger;
+    public float m_restartDelay = 5f;
+
+    #endregion
+
+
+    #region Main_Methods
+
+    void Awake()
+    {
+        m_anim = GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HungryScript.m_CurrentHungry <= 0)
+        {
+            HungryScript.Death();
+            m_restartTimer += Time.deltaTime;
+
+            if (m_restartTimer >= m_restartDelay)
+            {
+                Application.LoadLevel("title");
+            }
+            
+        }
+
+    } 
+
+    #endregion
+
+    #region Private_And_Protected
+
+    Animator m_anim;
+    float m_restartTimer;
+
+    #endregion
+}
